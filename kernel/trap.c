ac0b6e6bea0477bd9ae37f307dc148acc7f57657
Gracdjd <ziwenwang950@gmail.com>
2023-05-19 13:27:51 +0800

    trap.c

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,15 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else if()
+  }
+  //cow 的 page fault,当page fault kalloc一块内存,并将pte | PTE_W 
+  else if(r_scause() == 13 || r_scause() == 15)
+  {
+
+    uint64 va = r_stval();
+    pte_t *pte;
+    
+  }
   else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
